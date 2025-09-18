9604da983b7556c031a4689cdf81f0ad566f7e11
BlueWhistle2025 <operationsbluewhistle@gmail.com>
2025-09-18 14:46:51 +0530

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Carousel from "@/components/Carousel";
 import ExperienceSection from "@/components/ExperienceSection";
 import HomeContent from "@/components/HomeContent";
 import ProcessSection from "@/components/Process";
-import StoreLocator from "@/components/StoreLocator";
+// import StoreLocator from "@/components/StoreLocator";
 
 export default function Home() {
   return (
